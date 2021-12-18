7f8b0e12fcfa205c4e820f14550afd7bd8b3035f
Gamid-FrontEnd <g1a6m0i9d@gmail.com>
2021-12-18 14:08:58 +0200

    d

---
+++ menu/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./index.css";
+import MenuContainer from "./MenuContainer";
+
+
+ReactDOM.render(
+  <div>
+    <MenuContainer/>
+  </div>,
+  document.querySelector('#container')
+)
